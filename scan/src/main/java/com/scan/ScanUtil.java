50cbbb0216ca78eb73650f15f5df30cb9dc9e751
p_zhbgao <gaozhibin@shuame.mobile>
2019-04-19 16:17:30 +0800

    添加注释

--- scan/src/main/java/com/scan/ScanUtil.java
+++ scan/src/main/java/com/scan/ScanUtil.java
@@ -3,11 +3,19 @@ package com.scan;
 import android.os.Build;
 
 public class ScanUtil {
-
+    /**
+     * 捷宝硬解码
+     *
+     * @return
+     */
     public static boolean isJiebaoHard() {
         return Build.PRODUCT.equals("JIEBAO_HARD");
     }
 
+    /**
+     * 天波硬解码：TPS580C
+     * @return
+     */
     public static boolean isTPS580C() {
         return Build.PRODUCT.equals("rk3188");
     }
