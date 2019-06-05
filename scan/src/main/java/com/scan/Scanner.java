675ab8283d6541f77849694a4ca97633cf3e29cd
p_zhbgao <gaozhibin@shuame.mobile>
2019-06-05 15:52:24 +0800

    天波扫码解析

--- scan/src/main/java/com/scan/Scanner.java
+++ scan/src/main/java/com/scan/Scanner.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 
 /**
  */
@@ -116,6 +117,7 @@ public class Scanner {
 
     private class ReadThread extends Thread {
         private boolean stopRead = true;
+        byte[] readData;
 
         private ReadThread() {
             stopRead = false;
@@ -150,7 +152,33 @@ public class Scanner {
                         }
                     }
                 }
-                decode();
+                if (ScanUtil.isTPS580C()) {
+                    decodeTianBo();
+                } else if (ScanUtil.isJiebaoHard()) {
+                    decode();
+                }
+            }
+        }
+
+        private void decodeTianBo() {
+            InputStream mInputStream = serialPort.getInputStream();
+            int size;
+            try {
+                byte[] buffer = new byte[64];
+                size = mInputStream.read(buffer);
+                buffer = Arrays.copyOfRange(buffer, 0, size);
+                Log.d(TAG, "receive:" + toHexString(buffer));
+                readData = merge(readData, buffer);
+                String result = new String(readData);
+                Log.d(TAG, "merge:" + result);
+                if (readData != null && readData.length > 1
+                        && readData[readData.length - 2] == (byte) 0x0D
+                        && readData[readData.length - 1] == (byte) 0x0A) {
+                    readData = null;
+                    onDecode(result);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
 
@@ -183,5 +211,31 @@ public class Scanner {
                 var6.printStackTrace();
             }
         }
+    }
+
+    public static String toHexString(byte[] data) {
+        if (data == null) {
+            return "";
+        }
+        String string;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < data.length; i++) {
+            string = Integer.toHexString(data[i] & 0xFF);
+            if (string.length() == 1) {
+                stringBuilder.append("0");
+            }
+            stringBuilder.append(string.toUpperCase());
+        }
+        return stringBuilder.toString();
+    }
+
+    public static byte[] merge(byte[] hand, byte[] tail) {
+        if (hand == null) {
+            return tail;
+        }
+        byte[] data3 = new byte[hand.length + tail.length];
+        System.arraycopy(hand, 0, data3, 0, hand.length);
+        System.arraycopy(tail, 0, data3, hand.length, tail.length);
+        return data3;
     }
 }
